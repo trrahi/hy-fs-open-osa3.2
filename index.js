ea0ea7fa7f154400657592fd15a11d30a2501478
Tuomas Rahikka <tuomasrahikka3@gmail.com>
2024-08-10 16:06:36 +0300

    morgan

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const express = require("express")
-const app = express()
-
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
 
 // Middleware
-app.use(express.json())
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("🗿");
+  next();
+};
 
+morgan.token("kontsa", (req, res) => {
+  if (req.method == "POST") {
+    let thing = req.body
+    return JSON.stringify(thing)
+  }
+})
+
+
+app.use(express.json());
+// app.use(requestLogger);
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :kontsa")
+);
 
 let phonebook = [
   {
@@ -20,103 +39,104 @@ let phonebook = [
   {
     id: "3",
     name: "Dan Abramoc",
-    number: "12-43-234345"
+    number: "12-43-234345",
   },
   {
     id: "4",
     name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
+    number: "39-23-6423122",
+  },
 ];
-
-
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Olet juuressa")
+  res.send("Olet juuressa");
 });
 
 app.get("/api/persons", (req, res) => {
-    res.json(phonebook)
-    // res.send(phonebook)
-
+  res.json(phonebook);
 });
 
 app.get("/info", (req, res) => {
-  let numberOfPersons = phonebook.length
-  let info = `Phonebook has info for ${numberOfPersons} people`
-
-  let date = new Date().toString()
-  console.log(date);
-  res.send(info + "<br>" +  date)
+  let info = `Phonebook has info for ${phonebook.length} people`;
+  let date = new Date().toString();
+  res.send(info + "<br>" + date);
 });
 
 app.get("/api/persons/:id", (req, res) => {
   // console.log("req params", req.params);
-  const id = req.params.id
-  let person = phonebook.find(person => person.id === id)
+  const id = req.params.id;
+  let person = phonebook.find((person) => person.id === id);
   // console.log("id", id);
   // console.log("person", person);
 
   if (person) {
-    res.json(person)
+    res.json(person);
   } else {
-    res.status(404).end()
+    res.status(404).end();
   }
-})
+});
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id
-  let updatedPhonebook = phonebook.filter(person => person.id != id)
-  // console.log("updated phonebook", updatedPhonebook);
-  phonebook = updatedPhonebook
-  // console.log(phonebook);
-})
+  const id = req.params.id;
+  let updatedPhonebook = phonebook.filter((person) => {
+    // console.log(person);
+
+    return person.id !== id;
+  });
+  if (updatedPhonebook.length == phonebook.length) {
+    res.status(404).json({
+      error: "Henkilöä ei löytynyt",
+    });
+  } else {
+    res.status(200);
+  }
+  phonebook = updatedPhonebook;
+});
 
 app.post("/api/persons/:id", (req, res) => {
   let newId = Math.floor(Math.random() * 99999);
-  let reqBody = req.body
+  let reqBody = req.body;
 
-  let isPersonAlreadyThere = phonebook.find(person => person.name === reqBody.name) ? true : false
+  let isPersonAlreadyThere = phonebook.find((person) => person.name === reqBody.name)
+    ? true
+    : false;
 
   if (isPersonAlreadyThere) {
     return res.status(400).json({
-      error: "nimi pitää olla uniiikki!"
-    })
+      error: "nimi pitää olla uniikki!",
+    });
   }
 
-  
   if (reqBody.name && reqBody.number) {
-      let newPerson = {
-        id: newId,
-        name: reqBody.name,
-        number: reqBody.number,
-      };
-      console.log(newPerson);
-      phonebook = phonebook.concat(newPerson)
-      console.log("after concat", phonebook);
-      res.json(phonebook)
+    let newPerson = {
+      id: newId,
+      name: reqBody.name,
+      number: reqBody.number,
+    };
+    // console.log(newPerson);
+    phonebook = phonebook.concat(newPerson);
+    // console.log("after concat", phonebook);
+    res.json(phonebook);
   } else {
     res.status(400).json({
-      error: "ei sisältöä!"
-    })
+      error: "ei sisältöä!",
+    });
   }
+});
 
 
 
-})
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({
+    error:
+      "meow, this is an unknown endpoint. but what do i know, im just a smol cat",
+  });
+};
 
-app.get("/test", (req, res) => {
-
-  res.end()
-
-})
-
-
-
-
+app.use(unknownEndpoint);
 
 // Port
-const PORT = 3001
-app.listen(PORT)
+const PORT = 3001;
+app.listen(PORT);
 // console.log(`servu käynnis portis ${PORT}`);
