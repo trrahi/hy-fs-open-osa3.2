97fd7bef9ab9e67e26873eded662e0b00f7cdac9
Tuomas Rahikka <tuomasrahikka3@gmail.com>
2024-08-13 19:17:42 +0300

    back-endia laitettu toimimaan front-endin kanssa. CORS tuki lisätty

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors")
 
 // Middleware
+app.use(cors())
+
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
@@ -25,28 +28,30 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :kontsa")
 );
 
+
+
 let phonebook = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-523523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramoc",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+      {
+        id: "1",
+        name: "Arto Hellas",
+        number: "666 666 666 6666",
+      },
+      {
+        id: "2",
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+      },
+      {
+        id: "3",
+        name: "Dan Abramov",
+        number: "12-43-234345",
+      },
+      {
+        id: "4",
+        name: "Mary Poppendick",
+        number: "020202",
+      },
+]
 
 // Routes
 app.get("/", (req, res) => {
@@ -79,8 +84,9 @@ app.get("/api/persons/:id", (req, res) => {
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
+  console.log("id from backend: ", id);
   let updatedPhonebook = phonebook.filter((person) => {
-    // console.log(person);
+    console.log(person);
 
     return person.id !== id;
   });
@@ -89,14 +95,18 @@ app.delete("/api/persons/:id", (req, res) => {
       error: "Henkilöä ei löytynyt",
     });
   } else {
-    res.status(200);
+    console.log("backend, else happened");
+    res.status(200).end();
   }
   phonebook = updatedPhonebook;
+  console.log("After deleting a person, this is from the backend", phonebook);
+
 });
 
-app.post("/api/persons/:id", (req, res) => {
-  let newId = Math.floor(Math.random() * 99999);
+app.post("/api/persons", (req, res) => {
+  let newId = Math.floor(Math.random() * 9999999).toString()
   let reqBody = req.body;
+  // console.log("reqBody: ", reqBody);
 
   let isPersonAlreadyThere = phonebook.find((person) => person.name === reqBody.name)
     ? true
@@ -116,7 +126,7 @@ app.post("/api/persons/:id", (req, res) => {
     };
     // console.log(newPerson);
     phonebook = phonebook.concat(newPerson);
-    // console.log("after concat", phonebook);
+    // console.log("After adding a new person, this is from the backend", phonebook);
     res.json(phonebook);
   } else {
     res.status(400).json({
